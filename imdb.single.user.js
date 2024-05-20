6789c524ac793fb2dc8f873c7f64f488becd927e
lazuratus <lazur.nospam+github@gmail.com>
2024-05-19 22:40:45 -0400

    show multiple resolutions, group foreign releases, more configuration options, show releases you have using a local file, various other improvements

--- imdb.single.user.js
+++ imdb.single.user.js
@@ -2,18 +2,21 @@
 // @name		srrDB release lister for IMDB (single)
 // @icon		https://imdb.com/favicon.ico
 // @namespace	https://srrdb.com/
-// @downloadURL https://raw.githubusercontent.com/srrDB/srrextra/master/imdb.single.user.js
-// @updateURL	https://raw.githubusercontent.com/srrDB/srrextra/master/imdb.single.user.js
-// @version		0.4.3
-// @description Lists releases from srrdb.com on imdb.com
+// @downloadURL	https://github.com/srrDB/srrextra/raw/master/imdb.single.user.js
+// @updateURL	https://github.com/srrDB/srrextra/raw/master/imdb.single.user.js
+// @version		1.0
+// @description	Lists releases from srrdb.com on imdb.com
 // @author		Skalman
 // @author		Lazur
 // @match		https://imdb.com/title/*
 // @match		https://*.imdb.com/title/*
-// @require		https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js
+// @require		https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js
 // @require		https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @resource	CSS https://github.com/srrDB/srrextra/raw/master/imdb.single.user.css
+// @resource	HAVE file:///C:\path\to\list_of_releases_you_have.txt
 // @grant		GM_addStyle
 // @grant		GM_setClipboard
+// @grant		GM_getResourceText
 // ==/UserScript==
 
 /*global $,GM_addStyle,GM_setClipboard*/
@@ -25,19 +28,30 @@
 	console.clear();
 
 	// Configuration -----------------------------------------------------------
-	var resolution = '720p';
-	var showInternal = true;
-	var showHDTV = false;
+	const resolutions = ["720p", "1080p", "2160p"]; // Possible options: 720p, 1080p, 2160p, DVDRiP
+	const autoExpand = ["720p"];
+	const showOther = true; // Anything that isn't defined in resolutions ends up in Other
+	const groupedReleases = {};
 
-	var highlightHDTV = true;
-	var highlightProper = true;
-	var highlightInternal = true;
-	var highlightRemastered = true;
+	const showInternal = true;
+	const showHDTV = false;
 
-	var haveList = [""]; //a list of releases
+	const highlightKeywords = true;
+	const highlightBorderStyle = 'solid'; // Valid options: none, solid & dashed
 
-	var showForeign = false;
-	var highlightForeign = true;
+	const highlightHDTV = true;
+	const highlightReadNFO = true;
+	const highlightFixes = true;
+	const highlightUncut = false; // Includes UNCENSORED & UNRATED
+	const highlightProper = true; // Includes REPACK & RERIP
+	const highlightInternal = true;
+	const highlightRemastered = true;
+
+	const showForeign = true;
+	const groupForeign = true;
+	const autoExpandForeign = false; // If all releases for a resolution are foreign, they will be expanded even if set to false
+	const considerMultiAsForeign = true; // Releases containing MULTI will be treated as foreign even if they are not on srrDB
+	const highlightForeign = true;
 
 	const languagesToHighlight = [
 		'CZECH.DUAL', 'CZECH',
@@ -47,51 +61,39 @@
 		'FRENCH.QC', 'SUBFRENCH', 'TRUEFRENCH', 'FRENCH',
 		'GERMAN.DL', 'GERMAN.DUBBED.DL', 'GERMAN',
 		'iTALiAN',
+		'MULTI',
 		'NORWEGiAN',
 		'POLISH', 'PL.DUAL', 'PLDUB.DUAL', 'PLDUB',
-		'SPANiSH',
+		'LATiN.SPANiSH', 'SPANiSH',
 		'SWEDiSH',
 	];
-
-	var highlightColor = 'rgb(245, 197, 24)'; // default
-	var highlightHDTVColor = 'rgb(220, 20, 40)';
-	var highlightLanguageColor = 'rgb(127, 106, 252)';
 	// -------------------------------------------------------------------------
 
-	// Add styles
-	GM_addStyle(`.lnlBxO {align-items:start;}`);
-	GM_addStyle(`.srrdb-releases {--highlight-default:rgb(245,197,24);--highlight-hdtv:rgb(220,20,40);--highlight-foreign:rgb(127,106,252);background-color:rgba(255,255,255,0.08);border-radius:var(--ipt-cornerRadius);padding:4px 8px;}`);
-	GM_addStyle(`.srrdb-header {font-weight:bold;margin-bottom:0.5rem;}`);
-	GM_addStyle(`.srrdb-footer {font-weight:bold;margin-top:0.5rem;margin-bottom:1rem;text-align:right;}`);
-	GM_addStyle(`.srrdb-header .fa-external-link-alt,.srrdb-footer .fa-external-link-alt {margin-left:0.25rem;scale:0.7;}`);
-	GM_addStyle(`.release {white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-size:10pt;letter-spacing:-0.5px;display:block;}`);
-	GM_addStyle(`.release a {display:inline;border-radius:3px;padding:0 2px;}`);
-	GM_addStyle(`.highlight {background-color:rgba(0,0,0,0.15);box-shadow:inset 0px 0px 2px 1px rgba(0,0,0,0.75);border-radius:4px;border-style:solid;border-width:1px 0;border-color:var(--highlight-default);}`);
-	GM_addStyle(`.highlight-hdtv {border-color:var(--highlight-hdtv);}`);
-	GM_addStyle(`.highlight-foreign {border-color:var(--highlight-foreign);}`);
-	GM_addStyle(`.copy-release-name {display:inline-block;cursor:pointer;margin-right:3px;color:#fff!important;}`);
-	GM_addStyle(`.have-release-check {color:rgb(103,173,75);margin-right:1px;margin-left:2px;}`);
-	GM_addStyle(`.blink-text {animation:blinker 0.1s steps(1) infinite;}`);
-	GM_addStyle(`.blink-text .highlight {background:none;border:none;box-shadow:none;}`);
-	GM_addStyle(`@keyframes blinker {0% {color:#000;background-color:rgba(245,197,24,1);} 50% {color:rgb(245,197,24);background-color:rgba(245,197,24,0);} 100% {color:#000;background-color:rgba(245,197,24,1);}}`);
-	
+	const haveText = GM_getResourceText("HAVE");
+    const haveList = haveText.split('\n').map(line => line.trim()).filter(line => line);
+
+	const styles = GM_getResourceText("CSS");
+	GM_addStyle(styles);
+
 	var searchForeign = showForeign ? '' : 'foreign:no/';
 	var searchInternal = showInternal ? '' : '--internal/';
 	var searchHDTV = showHDTV ? '' : '--hdtv/';
 	var idPattern = /\d{7,8}/;
 	var imdbId = idPattern.exec(document.location.href);
 
-	var url = `https://api.srrdb.com/v1/search/imdb:${imdbId}/${searchForeign}category:x264/${resolution}/${searchInternal}${searchHDTV}--subfix/--nfofix`;
+	var url = `https://api.srrdb.com/v1/search/imdb:${imdbId}/${searchForeign}category:x264/${searchInternal}${searchHDTV}`;
+
+	console.log(url);
 
 	var html = `
 	<div data-testid="tm-box-up" class="srrdb-header sc-5766672e-0 gBbqMF">
 		<h3 class="ipc-title__text">Scene releases from <a class="ipc-link ipc-link--baseAlt" title="srrDB.com" target="_blank" href="https://www.srrdb.com/">srrDB.com<i class="fas fa-external-link-alt"></i></a></h3>
 	</div>
-	<div class="srrdb-releases mini-article">
+	<div class="srrdb-releases mini-article ${highlightKeywords ? 'highlight-keywords' : ''} highlight-border-${highlightBorderStyle}">
 		<span class="ab_widget">
-			<ul id="release-lister">
-				<li id="release-loading">Loading releases...</li>
-			</ul>
+			<div id="release-lister">
+				<span id="release-loading">Loading releases...</span>
+			</div>
 		</span>
 	</div>
 	<div class="srrdb-footer">
@@ -106,41 +108,135 @@
 	}).done(function (data) {
 		var releases = data.results;
 
+		resolutions.forEach(resolution => {
+			groupedReleases[resolution] = releases.filter(item => item.release.toLowerCase().includes(resolution.toLowerCase()));
+		});
+
+		if(showOther) {
+			groupedReleases['Other'] = [];
+
+			releases.forEach(release => {
+				let isOrphaned = true;
+				resolutions.forEach(resolution => {
+					if (groupedReleases[resolution].includes(release)) {
+						isOrphaned = false;
+					}
+				});
+
+				if (isOrphaned) {
+					groupedReleases['Other'].push(release);
+				}
+			});
+		}
+
 		if (releases.length > 0) {
 			$("#release-loading").remove();
 		} else {
-			$("#release-loading").text(`No ${resolution} release found...`);
+			$("#release-loading").text(`No ${resolutions.join(' / ')} release found...`);
 		}
 
-		$.each(releases, function (index, value) {
-			var releasename = value.release;
-			var url = `https://www.srrdb.com/release/details/${releasename}`;
+		$.each(groupedReleases, function(resolution, releases) {
+			if (releases.length > 0) {
 
-			var releaseNameText = releasename;
-			releaseNameText = highlightHDTV ? releaseNameText.replace(/(HDTV)/ig, '<span class="highlight highlight-hdtv">$1</span>') : releaseNameText;
+				let htmlBlock = `<div class="srrdb-block ${resolution}-block ${autoExpand.includes(resolution) ? 'expanded' : 'collapsed'}">
+				<span class="resolution-toggle" data-resolution="${resolution}"><h4><i class="fa-regular fa-square-minus"></i><i class="fa-regular fa-square-plus"></i> ${resolution} <span>(${releases.length})</span></h4></span>
+				<ul>`;
 
-			releaseNameText = highlightProper ? releaseNameText.replace(/(PROPER)/ig, '<span class="highlight">$1</span>') : releaseNameText;
-			releaseNameText = highlightInternal ? releaseNameText.replace(/(iNTERNAL)/ig, '<span class="highlight">$1</span>') : releaseNameText;
-			releaseNameText = highlightRemastered ? releaseNameText.replace(/(REMASTERED)/ig, '<span class="highlight">$1</span>') : releaseNameText;
+				let foreignReleasesBlock = ``;
 
-			for (const language of languagesToHighlight) {
-				const regex = new RegExp(`(${language})`, 'ig');
+				const foreignReleases = [];
 
-				if (regex.test(releaseNameText)) {
-					releaseNameText = highlightForeign ? releaseNameText.replace(regex, '<span class="highlight highlight-foreign">$1</span>') : releaseNameText;
-					break;
+				releases.forEach(item => {
+					let releasename = item.release;
+					let isForeign = (item.isForeign.toLowerCase() === "yes");
+					let url = `https://www.srrdb.com/release/details/${releasename}`;
+
+					if (!isForeign && considerMultiAsForeign) {
+						const multiPattern = /\.\d{4}(?:\.[^.]+)?\.MULT[Ii](?!.*?SUBS)\./;
+						if (multiPattern.test(item.release)) {
+							isForeign = true;
+						}
+					}
+					
+					if(isForeign) {
+						foreignReleases.push(item);
+					}
+
+					var haveReleaseCheck = '';
+					if (haveList.includes(releasename)) {
+						haveReleaseCheck = '<i class="fas fa-check-square have-release-check"></i>';
+					}
+
+					var releaseNameText = releasename;
+
+					const highlightRules = [
+						{ condition: highlightHDTV, regex: /\.(HDTV)\./ig, classSuffix: 'hdtv' },
+						{ condition: highlightReadNFO, regex: /\.(READ\.?NFO)\./ig, classSuffix: 'readnfo' },
+						{ condition: highlightUncut, regex: /\.(UNCUT|UNRATED|UNCENSORED)\./g, classSuffix: 'uncut' },
+						{ condition: highlightProper, regex: /\.(PROPER|REPACK|RERIP)\./ig, classSuffix: 'proper' },
+						{ condition: highlightInternal, regex: /\.(iNTERNAL|iNT)\./ig, classSuffix: 'internal' },
+						{ condition: highlightRemastered, regex: /\.(REMASTERED)\./ig, classSuffix: 'remastered' },
+						{ condition: highlightFixes, regex: /\.((DIR|SUB|NFO|SAMPLE|PROOF?)\.?FIX)\./ig, classSuffix: 'fix' }
+					];
+					
+					highlightRules.forEach(rule => {
+						if (rule.condition) {
+							releaseNameText = releaseNameText.replace(rule.regex, `.<span class="highlight highlight-${rule.classSuffix}">$1</span>.`);
+						}
+					});
+
+					$.each(languagesToHighlight, function(index, language) {
+						const regex = new RegExp(`\\.(${language})\\.(?!SUBS)`, 'ig');
+					
+						if (regex.test(releaseNameText)) {
+							releaseNameText = highlightForeign ? releaseNameText.replace(regex, '.<span class="highlight highlight-foreign">$1</span>.') : releaseNameText;
+							return false;
+						}
+					});
+
+					const commonHtmlBlock = `<li class="release ipc-link ipc-link--baseAlt" title="${releasename}"><i
+						class="ipc-link ipc-link--baseAlt copy-release-name fa-regular fa-copy"></i>${haveReleaseCheck}<a
+						class="ipc-link ipc-link--baseAlt" target="_blank" href="${url}">${releaseNameText}</a></li>`;
+				
+					if (!isForeign || !groupForeign) {
+						htmlBlock += commonHtmlBlock;
+					} else {
+						foreignReleasesBlock += commonHtmlBlock;
+					}
+
+				});
+
+				if(foreignReleases.length > 0 && groupForeign) {
+					const htmlForeignBlock = `<div class="srrdb-block foreign-${resolution}-block ${releases.length === foreignReleases.length || autoExpandForeign ? 'expanded' : 'collapsed'}">
+					<span class="foreign-toggle" data-foreign-resolution="${resolution}"><h4><i class="fa-regular fa-square-minus"></i><i class="fa-regular fa-square-plus"></i> Foreign <span>(${foreignReleases.length})</span></h4></span>
+					<ul>`;
+
+					htmlBlock += htmlForeignBlock;
+					htmlBlock += foreignReleasesBlock;
+					htmlBlock += `</ul></div>`
+
 				}
+
+				htmlBlock += `</ul></div>`;
+
+				$("#release-lister").append(htmlBlock);
+			
 			}
 
-			var haveReleaseCheck = '';
-			if (haveList.includes(releasename)) {
-				haveReleaseCheck = '<i class="fas fa-check-square have-release-check"></i>';
-			}
-
-			var repeatHtml = `<li class="release ipc-link ipc-link--baseAlt" title="${releasename}"><i class="ipc-link ipc-link--baseAlt copy-release-name far fa-copy"></i>${haveReleaseCheck}<a class="ipc-link ipc-link--baseAlt" target="_blank" href="${url}">${releaseNameText}</a></li>`;
-
-			$("#release-lister").append(repeatHtml);
 		});
+
+	});
+
+	$(document).on('click', '.resolution-toggle', function() {
+		var resolution = $(this).data('resolution');
+		var parentClass = '.' + resolution + '-block';
+		$(parentClass).toggleClass('collapsed expanded');
+	});
+
+	$(document).on('click', '.foreign-toggle', function() {
+		var resolution = $(this).data('foreign-resolution');
+		var parentClass = '.foreign-' + resolution + '-block';
+		$(parentClass).toggleClass('collapsed expanded');
 	});
 
 	$(document).on('click', '.copy-release-name', function (evt) {
@@ -152,4 +248,5 @@
 
 		evt.preventDefault();
 	});
+
 })();
